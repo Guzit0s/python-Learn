605ee34702afd9bd32d11d5da4ca59be6e6f9938
Guzit0s <gjbarreto15@gmail.com>
2024-08-24 18:42:26 -0300

    fiz e refiz melhorado!

--- mundo-2/Ex_050.py
+++ mundo-2/Ex_050.py
@@ -9,12 +9,28 @@
 #    print(f'{a}º termo é: {tt}')
 #    tt += r
 
-a1 = int(input('\nQual o valor primario da sua PA: '))
-r = int(input('Qual a razão da sua PA: '))
-tt = a1
+a1 = 0
+r = 0
 a = 0
+t = 0
 
-while a < 10:
-    a += 1
-    print(f'o {a}º termo é: {tt}')
-    tt += r
+while True:
+    a1 = int(input('\nDigite o valor primario da sua PA [ou 0 para sair]: '))
+    tt = a1
+    if a1 > 1:
+        r = int(input('Qual a razão da sua PA: '))
+        t = int(input('Quantos termos deseja?\n-> '))
+        for i in range(0, t):
+            a += 1
+            print(f'o {a}º termo é: {tt}')
+            tt += r
+        c = input('\nDeseja continuar?\n[S/N]: ').upper()
+        if c == 'S':
+            a = 0
+            continue
+        else:
+            print('\nAcabou')
+            break
+    else:
+        print('finalizando...')
+        break
